2cb54169bffef909afb746a1fe22bad0889b76c8
Oleksandr Dievri <o.dievri@outlook.com>
2019-03-02 15:51:42 +0200

    add nav

---
+++ src/components/Navbar.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-md bg-dark">
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <a className="nav-link" href="#">
+            Link 1
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">
+            Link 1
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">
+            Link 1
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
