6fecb9ca99a9832a9f2d752f47b7944e9171fe0f
Oleksandr Dievri <o.dievri@outlook.com>
2019-03-02 17:58:58 +0200

    Updates

---
+++ precache-manifest.a40de93dd5e163925e7951bc1b680028.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/silverkey-web/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "0b7c7a56a346d6a28eb2",
+    "url": "/silverkey-web/static/js/runtime~main.0b7c7a56.js"
+  },
+  {
+    "revision": "92ad3b706b6712eb8c62",
+    "url": "/silverkey-web/static/js/main.92ad3b70.chunk.js"
+  },
+  {
+    "revision": "f52862f29c403ab74283",
+    "url": "/silverkey-web/static/js/2.f52862f2.chunk.js"
+  },
+  {
+    "revision": "92ad3b706b6712eb8c62",
+    "url": "/silverkey-web/static/css/main.8c934334.chunk.css"
+  },
+  {
+    "revision": "b2bc9850514fdd967ed4aae243ca7423",
+    "url": "/silverkey-web/index.html"
+  }
+];
